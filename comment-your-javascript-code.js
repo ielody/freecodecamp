0eb9834655dcae3c3e6b40c84c41c168e9a63c20
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-04 11:02:03 +0200

    clean/add line break

--- comment-your-javascript-code.js
+++ comment-your-javascript-code.js
@@ -1,6 +1,7 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comment-your-javascript-code
+
 // This is my first comment
 
 /* This is a multi-line comment */
 
 // shortcut cmd + K + C
-
