db69bcae9f674caac2b3edc67c074d9ea443dced
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-05 17:38:29 +0200

    add age/firstName

---
+++ understanding-uninitialized-variables.js
@@ -0,0 +1,20 @@
+/*https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/understanding-uninitialized-variables*/
+
+let a = 5
+let b = 10
+let c = "I am a"
+
+a = a + 1
+b = b + 5
+c = c + "String"
+
+
+let age = 15
+let firstName = "My name is"
+
+age = age + 17
+firstName = firstName + " Ingeborg and I am"
+console.log(firstName, age)
+
+
+
