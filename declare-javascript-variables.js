b53f6700528d92f50d014b2c98810ad70ea82bce
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-05 10:57:55 +0200

    declared variable

---
+++ declare-javascript-variables.js
@@ -0,0 +1,5 @@
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/declare-javascript-variables
+
+//undefined, null, boolean, string, symbol, bigint, number, object
+
+var myName
