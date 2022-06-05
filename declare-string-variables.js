d3b23d1ba78823d4da582aec27a56c5143524c3d
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-05 12:46:30 +0200

    add name variables

---
+++ declare-string-variables.js
@@ -0,0 +1,6 @@
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/declare-string-variables
+
+let myFirstName = "Ingeborg"
+let myLastName = "Eldøy"
+
+let profession = "Student"
