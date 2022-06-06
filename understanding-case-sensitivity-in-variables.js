99a6ea7a9c06a0f5f566cf7f7a6d72a925af3e7f
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-06 18:25:14 +0200

    case sensitivity

---
+++ understanding-case-sensitivity-in-variables.js
@@ -0,0 +1,12 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/understanding-case-sensitivity-in-variables */
+
+// Variable declarations
+var studlyCapVar
+var properCamelCase
+var titleCaseOver
+
+// Variable assignments
+var studlyCapVar = 10
+var properCamelCase = "A String"
+titleCaseOver = 9000
+
