bccdeb6d6153382735d56bc34ca6fc05619552be
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-06 19:01:03 +0200

    changed to uppercase

--- declare-a-read-only-variable-with-the-const-keyword.js
+++ declare-a-read-only-variable-with-the-const-keyword.js
@@ -1,11 +1,11 @@
 /* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/declare-a-read-only-variable-with-the-const-keyword */
 
-const fCC = "freeCodeCamp"
+const FCC = "freeCodeCamp"
 let fact = "is cool!"
 fact = "is awesome!"
-console.log(fCC, fact)
+console.log(FCC, fact)
 
-const hotelName = "Hilton"
+const HOTEL = "Hilton"
 let location = "in Amsterdam"
 location = "in Belgium"
-console.log(hotelName, location)
+console.log(HOTEL, location)
