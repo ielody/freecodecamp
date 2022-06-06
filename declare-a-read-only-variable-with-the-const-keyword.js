7c0804d3a0e82d4008d55ffef25ff5d3622bf444
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-06 18:53:41 +0200

    const/let

---
+++ declare-a-read-only-variable-with-the-const-keyword.js
@@ -0,0 +1,11 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/declare-a-read-only-variable-with-the-const-keyword */
+
+const fCC = "freeCodeCamp"
+let fact = "is cool!"
+fact = "is awesome!"
+console.log(fCC, fact)
+
+const hotelName = "Hilton"
+let location = "in Amsterdam"
+location = "in Belgium"
+console.log(hotelName, location)
