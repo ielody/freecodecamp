51ff18c923c4c87cc2648a618aed0d329419b116
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 16:39:10 +0200

    res 2 %, Math.floor

---
+++ finding-a-remainder-in-javascript.js
@@ -0,0 +1,8 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/finding-a-remainder-in-javascript */
+
+const remainder = 11 % 3
+//answer 2
+
+const rem = Math.floor(5 / 2)
+
+console.log(rem, remainder)
