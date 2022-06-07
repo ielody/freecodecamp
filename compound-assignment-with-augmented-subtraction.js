c3c1a694d185711faf5033dffb01e8f0e174a9dc
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 17:43:57 +0200

    -= operator

---
+++ compound-assignment-with-augmented-subtraction.js
@@ -0,0 +1,11 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-subtraction */
+
+let a = 11
+let b = 9
+let c = 3
+
+a -= 6
+b -= 15
+c -= 1
+
+console.log(a,b,c)
