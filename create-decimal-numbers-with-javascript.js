2a6da66fbb48105b4c44b8c4e7d7a7ff4412d750
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 15:42:31 +0200

    decimal nr

---
+++ create-decimal-numbers-with-javascript.js
@@ -0,0 +1,4 @@
+/*https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/create-decimal-numbers-with-javascript */
+
+const myDecimal = 5.7
+console.log(myDecimal)
