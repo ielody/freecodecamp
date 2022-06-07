d39e7d4b70b08c4ef92ed949a7b5d4e9c53ad072
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 15:29:11 +0200

    decrement -- to 10

---
+++ decrement-a-number-with-javascript.js
@@ -0,0 +1,8 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/decrement-a-number-with-javascript */
+
+let myVar = 11
+
+myVar--
+
+console.log(myVar)
+//result 10
