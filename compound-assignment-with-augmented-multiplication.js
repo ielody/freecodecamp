7d921cc38dd42dc6e11359081ffb1b62188c2ad0
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 17:49:48 +0200

    *+ operator

---
+++ compound-assignment-with-augmented-multiplication.js
@@ -0,0 +1,11 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-multiplication */
+
+let a = 5
+let b = 12
+let c = 4.6
+
+a *= 5
+b *= 3
+c *= 10
+
+console.log(a,b,c)
