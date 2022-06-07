cad0ff4b7dee46aa0e4bd1a1f91ab73a2c586c1a
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 15:55:04 +0200

    div decimal

---
+++ divide-one-decimal-by-another-with-javascript.js
@@ -0,0 +1,7 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/divide-one-decimal-by-another-with-javascript */
+
+const quotient = 4.4 / 2.0
+console.log(quotient)
+
+const divQuotient = 5.2 / 3.1
+console.log(divQuotient)
