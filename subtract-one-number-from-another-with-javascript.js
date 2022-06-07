5dc46c64d77f719a8f9eaadf7463df6627b0ef07
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 13:07:36 +0200

    add 2/3 numbers

---
+++ subtract-one-number-from-another-with-javascript.js
@@ -0,0 +1,7 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/subtract-one-number-from-another-with-javascript */
+
+const difference = 45 - 33
+console.log(difference)
+
+const sum = 45 + 23 + 47
+console.log(sum)
