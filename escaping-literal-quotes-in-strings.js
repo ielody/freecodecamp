5cacf4ddfd9e4e29f3d2033da754b12404ffb430
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 18:23:52 +0200

    quotes within quotes

---
+++ escaping-literal-quotes-in-strings.js
@@ -0,0 +1,8 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/escaping-literal-quotes-in-strings */
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."
+
+const bil = "Tore drives a \"Volvo\" when he goes on \"road-trips\"."
+
+console.log(myStr, bil)
+
