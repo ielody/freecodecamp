2469cb1b0f3f4845b93938ad4a2f529db6df09a8
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 15:47:56 +0200

    decimal multiply

---
+++ multiply-two-decimals-with-javascript.js
@@ -0,0 +1,10 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/multiply-two-decimals-with-javascript */
+
+const product = 2.0 * 2.5
+console.log(product)
+//5
+
+const decimal = 15.0 * 2.3
+console.log(decimal)
+//34.5
+
