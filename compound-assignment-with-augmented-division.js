7d979bda72e6f3e32ef54909b8797bf92a9dab11
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 17:58:32 +0200

    /= operator

---
+++ compound-assignment-with-augmented-division.js
@@ -0,0 +1,11 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-division */
+
+let a = 48
+let b = 108
+let c = 33
+
+a /= 12
+b /= 4
+c /= 11
+
+console.log(a,b,c)
