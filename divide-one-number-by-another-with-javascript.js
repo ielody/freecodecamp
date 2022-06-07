02a8f5ff3f8c8a1970ebc3f4e678ce4a29d17d95
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 13:32:00 +0200

    divide quotient

---
+++ divide-one-number-by-another-with-javascript.js
@@ -0,0 +1,5 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/divide-one-number-by-another-with-javascript */
+
+const quotient = 66 / 33
+console.log(quotient)
+
