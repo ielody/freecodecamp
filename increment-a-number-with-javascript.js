caf0401e9638f986bc202a30a71be5d5c814876b
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 15:11:10 +0200

    increment two nr

---
+++ increment-a-number-with-javascript.js
@@ -0,0 +1,13 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/increment-a-number-with-javascript */
+
+let myVar = 87
+
+myVar++
+
+console.log(myVar)
+
+let biler = 99
+
+biler++
+
+console.log(biler)
