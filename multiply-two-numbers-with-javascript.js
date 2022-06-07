7c75356b448d84649fdca36f4053f85611434422
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-07 13:23:58 +0200

    2/3 nr multiply

---
+++ multiply-two-numbers-with-javascript.js
@@ -0,0 +1,7 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/multiply-two-numbers-with-javascript */
+
+const product = 8 * 10
+console.log(product)
+
+const sum = 8 * 10 * 30
+console.log(sum)
