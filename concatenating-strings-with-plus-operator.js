294f3a3309aad631f5cb35c231b688d62e51051c
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-08 23:57:08 +0200

    two sentences

---
+++ concatenating-strings-with-plus-operator.js
@@ -0,0 +1,5 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/concatenating-strings-with-plus-operator */
+
+const myStr = "This is the start. " + "This is the end."
+
+console.log(myStr)
