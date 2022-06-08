ba298282cf0456c981620ef2899d968b4b23bcc0
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-08 23:32:07 +0200

    1.,2.,3.line

---
+++ escape-sequences-in-strings.js
@@ -0,0 +1,5 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/escape-sequences-in-strings */
+
+const myStr = "FirstLine, \n\t\\SecondLine, \n\ThirdLine"
+
+console.log(myStr)
