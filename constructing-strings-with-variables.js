3072bfa2728f799ab104a009dc49a7629aae6bf0
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-09 00:29:56 +0200

    making strings variables

---
+++ constructing-strings-with-variables.js
@@ -0,0 +1,11 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/constructing-strings-with-variables */
+
+const myName = "Ingeborg"
+const myStr = "Hello, my name is " + myName + ", how are you?"
+
+console.log(myStr)
+
+const cafe = "Coffee Company "
+const favorite = "My favorite cafe is " + cafe + "because of the delicious coffee."
+
+console.log(favorite)
