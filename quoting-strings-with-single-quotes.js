6c434d78476acb08117d67ce0ddca43165b7ff12
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-08 23:11:52 +0200

    link a quote

---
+++ quoting-strings-with-single-quotes.js
@@ -0,0 +1,5 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/quoting-strings-with-single-quotes */
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>'
+
+console.log(myStr)
