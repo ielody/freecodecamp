11b534593423230460f0143b67696af79d2eac35
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-09 16:32:28 +0200

    first letter of name

---
+++ use-bracket-notation-to-find-the-first-character-in-a-string.js
@@ -0,0 +1,10 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-bracket-notation-to-find-the-first-character-in-a-string */
+
+//bracket notation to find 1. char in string.
+
+let firstLetterOfLastName = ""
+const lastName = "Lovelace"
+
+firstLetterOfLastName = lastName[0]
+
+console.log(firstLetterOfLastName)
