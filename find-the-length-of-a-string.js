8ce64fa2bae2546398d56f3b8c097408778454ea
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-09 13:09:08 +0200

    length of name

---
+++ find-the-length-of-a-string.js
@@ -0,0 +1,8 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/find-the-length-of-a-string */
+
+//let lastNameLength = 0
+const lastName = "LoveLace"
+
+lastNameLength = lastName.length
+
+console.log(lastNameLength)
