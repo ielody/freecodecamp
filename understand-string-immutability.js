f3769929f3ac6ab750a6af518775e94f869ed684
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-09 17:46:04 +0200

    immutable strings

---
+++ understand-string-immutability.js
@@ -0,0 +1,8 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/understand-string-immutability */
+
+//immutable strings cannot be altered or created, needs to assign it to a new
+
+let string = "Jello World"
+string = "Hello World"
+
+console.log(string)
