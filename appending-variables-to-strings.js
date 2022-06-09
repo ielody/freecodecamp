7c74e42ad3194f36a58f0ad450289adaa20c256e
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-09 10:23:01 +0200

    season/spring-append

---
+++ appending-variables-to-strings.js
@@ -0,0 +1,12 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/appending-variables-to-strings */
+
+const someAdjective = "fun"
+let myStr = "Learning to code is "
+const string = myStr += someAdjective
+
+console.log(string)
+
+const season = "favorite season"
+let spring = "Spring is my "
+
+console.log(spring += season)
