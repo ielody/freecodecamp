49e5cc55cba7bf63c003219876e8d160d5058abb
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-10 15:15:20 +0200

    string, nr array

---
+++ store-multiple-values-in-one-variable-using-javascript-arrays.js
@@ -0,0 +1,4 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/store-multiple-values-in-one-variable-using-javascript-arrays */
+
+const myArray = ['apple', 2]
+console.log(myArray)
