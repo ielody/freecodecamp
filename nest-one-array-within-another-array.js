3aa0c9f54ccc874978a021248706618c58b1670d
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-10 15:21:58 +0200

    nested array, two items

---
+++ nest-one-array-within-another-array.js
@@ -0,0 +1,7 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/nest-one-array-within-another-array */
+
+//multidimensional array, nested array
+
+const myArray = [["Cucumber", 12], ["Tomatoes", 53]]
+
+console.log(myArray)
