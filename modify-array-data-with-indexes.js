ba471ab2eef35301b5d878fc3ae7bef4dc66d6b7
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-10 16:52:56 +0200

    modify arrays

---
+++ modify-array-data-with-indexes.js
@@ -0,0 +1,11 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/modify-array-data-with-indexes */
+
+const myArray = [18, 64, 99]
+myArray[0] = 45
+
+console.log(myArray)
+
+const list = ["yoga", "pilates", "meditation"]
+list[1] = "ukulele"
+
+console.log(list)
