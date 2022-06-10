c9b017e3f646bcc7064ca9417ab2defe299496bf
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-10 15:01:54 +0200

    madlib

---
+++ word-blanks.js
@@ -0,0 +1,10 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/word-blanks */
+
+//Fill in the blanks, "madlib"
+
+const myNoun = "dog"
+const myAdjective = "big"
+const myVerb = "ran"
+const myAdverb = "quickly"
+
+const wordBlanks = `My ${myNoun} is ${myAdjective} and ${myVerb} ${myAdverb} over the meadow`
