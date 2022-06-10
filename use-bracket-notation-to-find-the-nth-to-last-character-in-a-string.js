5ee83912baf2d2e51093c1605875ece2ae1023a6
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-10 11:37:29 +0200

    nth to last character in string

---
+++ use-bracket-notation-to-find-the-nth-to-last-character-in-a-string.js
@@ -0,0 +1,7 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-bracket-notation-to-find-the-nth-to-last-character-in-a-string */
+
+const lastName = "LoveLace"
+
+const secondToLastLetter = lastName[lastName.length - 3]
+
+console.log(secondToLastLetter)
