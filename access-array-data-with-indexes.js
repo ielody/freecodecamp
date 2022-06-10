aa5e6604a5d87ca81a549378d97f1b807960eaa3
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-10 15:53:57 +0200

    access array data

---
+++ access-array-data-with-indexes.js
@@ -0,0 +1,11 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/access-array-data-with-indexes */
+
+const myArray = [50, 60, 70]
+const myData = myArray[0]
+
+console.log(myData)
+
+const shopping = ["cucumber", 23, "tomatoes", 56, "strawberries box", 2]
+const arrayData = shopping[3]
+
+console.log(arrayData)
