2bff158fcd1bfa44f9d899863dc1141c3ea707e5
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-10 11:19:50 +0200

    find 3rd letter in string

---
+++ use-bracket-notation-to-find-the-nth-character-in-a-string.js
@@ -0,0 +1,7 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-bracket-notation-to-find-the-nth-character-in-a-string */
+
+const lastName = "LoveLace"
+
+const thirdLetterOfLastName = lastName[2]
+
+console.log(thirdLetterOfLastName)
