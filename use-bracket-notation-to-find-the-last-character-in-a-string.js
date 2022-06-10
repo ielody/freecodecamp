67fc2122811abd6342849ee774f447f598350410
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-10 11:28:00 +0200

    find last letter in name

---
+++ use-bracket-notation-to-find-the-last-character-in-a-string.js
@@ -0,0 +1,8 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-bracket-notation-to-find-the-last-character-in-a-string */
+
+const lastName = "Lovelace"
+
+const lastLetter = lastName[lastName.length - 1]
+
+console.log(lastLetter)
+
