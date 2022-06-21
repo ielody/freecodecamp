4cf8cdf3709eddbb9f0464ada56c9dfbb15f3e32
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-21 10:59:20 +0200

    using pop

---
+++ manipulate-arrays-with-pop.js
@@ -0,0 +1,17 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-pop */
+
+//pop removes the last element from an array and returns that element
+
+const myArray = [["John", 23], ["cat", 2]]
+
+const popArray = myArray.pop()
+
+// console.log(myArray)
+// console.log(popArray)
+
+const nameArray = ["Karen", "Rolf", "Ellen"]
+const lastName = nameArray.pop()
+
+console.log(lastName)
+console.log(nameArray)
+
