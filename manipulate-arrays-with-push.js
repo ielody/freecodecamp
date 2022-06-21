3126c195710a3f063bf5d4ed288d1e07c0aed1f9
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-21 10:19:04 +0200

    push vegetables

---
+++ manipulate-arrays-with-push.js
@@ -0,0 +1,13 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-push */
+
+
+const myArray = [["John", 23], ["cat", 2]]
+
+myArray.push("dog", 3)
+
+console.log(myArray)
+
+const pushStuff = ["carrots", 45, "cucumbers", 67]
+pushStuff.push("apples", 34)
+
+console.log(pushStuff)
