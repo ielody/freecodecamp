57a33a8e7b921f0ef10b8d27923bf0a09722439b
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-23 16:18:19 +0200

    shopping list

---
+++ shopping-list.js
@@ -0,0 +1,5 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/shopping-list */
+
+const myList = [["Bananas", 5],["milkshake", 2], ["apples", 7], ["cheeses", 3], ["grapefruits", 4]]
+
+console.log(myList)
