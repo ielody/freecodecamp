aae4ca7d7321c0469b8b06065e720be7de3493ce
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-23 16:44:45 +0200

    param1 + param2

---
+++ passing-values-to-functions-with-arguments.js
@@ -0,0 +1,6 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/passing-values-to-functions-with-arguments */
+
+function functionWithArgs(param1, param2) {
+console.log(param1 + param2)
+}
+functionWithArgs(2,3)
