0172275e31f5cab6b2a9f46b082d493d238dbef6
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-24 21:29:05 +0200

    return function num

---
+++ return-a-value-from-a-function-with-return.js
@@ -0,0 +1,14 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/return-a-value-from-a-function-with-return */
+
+function timesFive(num) {
+  return num * 5
+}
+console.log(timesFive(5))
+//25
+
+function divTen(num) {
+  return num / 10
+}
+console.log(divTen(98))
+
+
