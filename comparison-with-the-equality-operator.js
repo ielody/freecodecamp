be3e8e7e5fca38b5efe7ac4b17ca8708ee719b2b
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-27 21:30:18 +0200

    if == num

---
+++ comparison-with-the-equality-operator.js
@@ -0,0 +1,10 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-equality-operator */
+
+
+function testEqual(val) {
+  if(val == 12) {
+    return "Equal"
+  }
+  return "Not Equal"
+}
+console.log(testEqual(12))
