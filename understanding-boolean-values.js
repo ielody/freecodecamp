88c6ca68861cee536e7054ef6a30e2d14ad0f16e
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-27 21:00:51 +0200

    boolean true

---
+++ understanding-boolean-values.js
@@ -0,0 +1,7 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/understanding-boolean-values */
+
+function welcomeToBooleans() {
+  return true
+}
+
+console.log(welcomeToBooleans())
