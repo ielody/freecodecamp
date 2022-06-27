43425d8b694c47f2442d7e0729a6e6b7b2ecc9c9
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-27 23:59:23 +0200

    comparing a and b

---
+++ practice-comparing-different-values.js
@@ -0,0 +1,10 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/practice-comparing-different-values */
+
+function compareEquality(a, b) {
+  if (a == b) {
+    return "Equal"
+  }
+  return "Not Equal"
+}
+
+console.log(compareEquality(10, 9))
