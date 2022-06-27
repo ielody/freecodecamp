76f34808c28d6a09c4a84f95f0a348453169bbda
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-27 21:16:54 +0200

    if true\false

---
+++ use-conditional-logic-with-if-statements.js
@@ -0,0 +1,11 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-conditional-logic-with-if-statements */
+
+function trueOrFalse(wasThatTrue) {
+  if (wasThatTrue) {
+    return "Yes, that was true"
+  }
+  return "No, that was false"
+}
+
+console.log(trueOrFalse(true))
+console.log(trueOrFalse(false))
