cdaa9bf2668b90624be78928dc56f9742bb2058e
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-28 07:08:50 +0200

    comparing diff values

--- practice-comparing-different-values.js
+++ practice-comparing-different-values.js
@@ -1,10 +1,11 @@
 /* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/practice-comparing-different-values */
 
 function compareEquality(a, b) {
-  if (a == b) {
+  if (a === b) {
     return "Equal"
   }
   return "Not Equal"
 }
-
+console.log(compareEquality(10, "10"))
+console.log(compareEquality(10, 10))
 console.log(compareEquality(10, 9))
