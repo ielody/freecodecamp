161a3a3fd6e209bd8214eb0a9c43a2031f2c2a5b
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-28 11:37:54 +0200

    else if

---
+++ introducing-else-if-statements.js
@@ -0,0 +1,17 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/introducing-else-if-statements */
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10"
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5"
+
+  } else {
+    return "Between 5 and 10"
+  }
+}
+
+console.log(testElseIf(7))
+console.log(testElseIf(4))
