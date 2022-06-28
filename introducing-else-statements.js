bece4f438d69c19319abe9731e47c5a62799df87
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-28 10:49:42 +0200

    testing else

---
+++ introducing-else-statements.js
@@ -0,0 +1,30 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/introducing-else-statements */
+
+function testElse(val) {
+
+  if (val > 5) {
+    return "Bigger than 5"
+  }
+
+  else {
+    return "5 or Smaller"
+  }
+}
+
+console.log(testElse(4))
+
+
+function testElse(val) {
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  return result;
+  }
+
+  console.log(testElse(7))
+
