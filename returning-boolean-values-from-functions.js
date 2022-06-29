9f8f933a6d65491bdc83d8cd6ca19ce523dd11fc
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-29 11:36:16 +0200

    boolean less than

---
+++ returning-boolean-values-from-functions.js
@@ -0,0 +1,8 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/returning-boolean-values-from-functions */
+
+function isLess(a, b) {
+  return (a < b)
+}
+
+console.log(isLess(10, 15))
+console.log(isLess(12, 11))
