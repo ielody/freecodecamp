22c385c75fa47dfa48c9743b5dfa8f269ece2a0b
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-30 17:17:00 +0200

    making an object

---
+++ build-javascript-objects.js
@@ -0,0 +1,10 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/build-javascript-objects */
+
+const myDog = {
+  "name": "Pondus",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Poppy", "Daisy"]
+}
+
+console.log(myDog)
