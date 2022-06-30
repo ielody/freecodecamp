3a77d024ee8d831c00212d957af7450d1674c31e
Ingeborg Eldøy <ieldoy@gmail.com>
2022-06-30 17:41:06 +0200

    dot notation object

---
+++ accessing-object-properties-with-dot-notation.js
@@ -0,0 +1,12 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/accessing-object-properties-with-dot-notation */
+
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+}
+
+const hatValue = testObj.hat
+const shirtValue = testObj.shirt
+
+console.log(hatValue, shirtValue)
