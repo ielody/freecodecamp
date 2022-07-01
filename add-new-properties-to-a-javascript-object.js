de96b80758a5c1e15d24946f57f6f595d6a1b7db
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-01 16:02:33 +0200

    add new obj properties

---
+++ add-new-properties-to-a-javascript-object.js
@@ -0,0 +1,14 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/add-new-properties-to-a-javascript-object */
+
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof"
+
+myDog.foods = ["Cucumber, carrrots"]
+
+console.log(myDog)
