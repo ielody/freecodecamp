e6ed849daa217a41122a3de3bf3a2d3c9b750c03
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-01 14:24:45 +0200

    update object prop

---
+++ updating-object-properties.js
@@ -0,0 +1,11 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/updating-object-properties */
+
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+}
+
+myDog.name = "Happy Coder"
+console.log(myDog)
