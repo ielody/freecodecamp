2d4c256664fd0f1ecda90376f6edce5f81a0486d
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-02 08:34:45 +0200

    complex objects

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,27 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulating-complex-objects */
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+
+  {
+  "artist": "Destiny's child",
+  "title": "Independent woman",
+  "release_year": 2003,
+  "formats": [
+    "CD",
+      "8T",
+      "LP"
+  ]
+}]
+
+console.log(myMusic)
