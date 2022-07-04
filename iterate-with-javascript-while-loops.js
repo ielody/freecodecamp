dab1b608803b05b5e6a0a477309d031bc0aeceff
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-04 18:49:29 +0200

    reversed while loop

---
+++ iterate-with-javascript-while-loops.js
@@ -0,0 +1,13 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-with-javascript-while-loops */
+
+//while runcs the same code many times until it is no longer true.
+
+const myArray = []
+let i = 5
+
+while (i >= 0) {
+  myArray.push(i)
+  i--
+}
+
+console.log(myArray)
