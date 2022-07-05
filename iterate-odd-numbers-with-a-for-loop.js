c4c10c41ff1eb7aca4d0c7bb08e8a81aadec7c61
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-05 23:23:24 +0200

    odd numbers for loop

---
+++ iterate-odd-numbers-with-a-for-loop.js
@@ -0,0 +1,8 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-odd-numbers-with-a-for-loop */
+
+const myArray = []
+
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i)
+}
+console.log(myArray)
