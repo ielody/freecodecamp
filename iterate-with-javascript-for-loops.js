59e72bab22df7d06a741054e9be5c081c70d7538
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-05 18:23:52 +0200

    for loop

---
+++ iterate-with-javascript-for-loops.js
@@ -0,0 +1,11 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-with-javascript-for-loops */
+
+//A for loop runs a specific amount of time
+
+const myArray = []
+
+for (let i = 1; i < 6; i++) {
+  myArray.push(i)
+}
+
+console.log(myArray)
