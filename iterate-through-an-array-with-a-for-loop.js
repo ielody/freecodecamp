1fce67c7ae0bc9e170f0e28eefb06d28a3304994
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-06 16:14:44 +0200

    total + myArr

---
+++ iterate-through-an-array-with-a-for-loop.js
@@ -0,0 +1,9 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-through-an-array-with-a-for-loop */
+
+const myArr = [2,3,4,5,6]
+
+let total = 0
+
+for (let i = 0; i < myArr.length; i++) {
+  console.log(total += myArr[i])
+}
