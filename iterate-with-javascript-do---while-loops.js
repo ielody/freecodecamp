fc27b35c134f8a7ec239755de2d8a76604bdebf8
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-11 13:50:30 +0200

    do while loop

---
+++ iterate-with-javascript-do---while-loops.js
@@ -0,0 +1,25 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-with-javascript-do---while-loops */
+
+//first do first pass of the code, then while condition
+
+// let myArray = []
+// let i = 10
+
+//   do {
+//     myArray.push(i)
+//     i++
+
+//   } while (i <= 10)
+
+// console.log(i)
+
+let myArra = []
+let b = 75
+
+  do {
+    myArra.push(b)
+    b++
+
+  } while (b <= 100)
+
+console.log(b)
