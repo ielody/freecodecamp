d1333d7eed79720cc3793684a9aed4590f0249d1
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-15 15:49:36 +0200

    Math.floor/random

---
+++ generate-random-whole-numbers-with-javascript.js
@@ -0,0 +1,7 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/generate-random-whole-numbers-with-javascript */
+
+function randomWholeNum() {
+  return Math.floor(Math.random() * 222.23)
+}
+const result = randomWholeNum()
+console.log(result)
