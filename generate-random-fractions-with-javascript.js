8bf5280dd9a78d91cca1678210d3191dcabe550f
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-15 15:32:41 +0200

    Math.random

---
+++ generate-random-fractions-with-javascript.js
@@ -0,0 +1,7 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/generate-random-fractions-with-javascript */
+
+function randomFraction() {
+  return Math.random()
+}
+const result = randomFraction()
+console.log(result)
