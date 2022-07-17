b3dae25bd0f3324635855f26fb5de4e08d96cfd0
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-17 10:02:03 +0200

    parseInt str 56

---
+++ use-the-parseint-function.js
@@ -0,0 +1,8 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-parseint-function */
+
+function convertToInteger(str) {
+  return parseInt(str)
+}
+
+const result = convertToInteger("56")
+console.log(result)
