0e38299652e371ee9cf39a4166526826a362f55e
Ingeborg Eldøy <ieldoy@gmail.com>
2022-07-21 17:23:07 +0200

    Singl ternary operator

---
+++ use-the-conditional-ternary-operator.js
@@ -0,0 +1,8 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-conditional-ternary-operator */
+
+function checkEqual(a, b) {
+  return a === b ? "Equal" : "Not equal"
+}
+
+console.log(checkEqual(1, 2))
+
