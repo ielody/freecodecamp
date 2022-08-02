f8887b35e531c48ee2d6ece2243c0285848afeb5
Ingeborg Eldøy <ieldoy@gmail.com>
2022-08-02 23:00:03 +0200

    spread operator copy array

---
+++ use-the-spread-operator-to-evaluate-arrays-in-place.js
@@ -0,0 +1,7 @@
+/* https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/es6/use-the-spread-operator-to-evaluate-arrays-in-place */
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY']
+let arr2
+arr2 = [...arr1]
+
+console.log(arr2)
